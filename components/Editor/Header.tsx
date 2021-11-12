6934cdb3a7899ee788d4917f3c7724433d61adf9
Tair Asim <tair@hey.com>
2021-11-12 01:14:22 +0100

    chore: Fix execution on London fork and update @ethereum packages
    - London fork requires a base fee, so use the @ethereumjs/block to
      set the EIP-1559 Block for transaction run.
      See: https://github.com/ethereumjs/ethereumjs-monorepo/tree/master/packages/block#eip-1559-blocks
    - Update to the latest @etherumjs packages
    - Reinit Common on chain/fork change

--- components/Editor/Header.tsx
+++ components/Editor/Header.tsx
@@ -41,7 +41,7 @@ const EditorHeader = ({
     <div className="flex justify-between items-center w-full">
       <h3 className="font-semibold text-md hidden xl:block">
         EVM Playground
-        <Label>{selectedFork}</Label>
+        <Label>{selectedFork?.name}</Label>
       </h3>
 
       <div className="flex items-center justify-between w-full xl:w-auto">
