f6c13f564ea5854cd7730c10dec578425bafa3c7
jonahcui <jonahcui0@gmail.com>
2022-07-05 10:33:39 +0800

    [feat] add auto scroll for console.

--- components/Editor/Console.tsx
+++ components/Editor/Console.tsx
@@ -1,3 +1,5 @@
+import { useEffect, useRef } from 'react'
+
 import { toKeyIndex } from 'util/string'
 
 import { IConsoleOutput } from './types'
@@ -7,8 +9,21 @@ type Props = {
 }
 
 const Console = ({ output }: Props) => {
+  const container = useRef<HTMLDivElement>(null)
+  const endDiv = useRef<HTMLDivElement>(null)
+
+  useEffect(() => {
+    container.current?.parentElement?.scrollTo({
+      top: endDiv.current?.offsetTop,
+      behavior: 'smooth',
+    })
+  }, [output])
+
   return (
-    <div className="px-4 pt-2 leading-5 font-mono text-tiny text-gray-400 dark:text-gray-500">
+    <div
+      ref={container}
+      className="px-4 pt-2 leading-5 font-mono text-tiny text-gray-400 dark:text-gray-500"
+    >
       {output.map((log, index) => (
         <pre key={toKeyIndex('line', index)}>
           {log.type === 'error' && (
@@ -20,6 +35,7 @@ const Console = ({ output }: Props) => {
           {log.message}
         </pre>
       ))}
+      <div ref={endDiv}></div>
     </div>
   )
 }
