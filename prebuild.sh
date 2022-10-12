03ff0cefa4ff690278b4d0094840ec828d9527cd
Tal Vaizman <tal@comitylabs.com>
2022-10-12 03:07:08 +0300

    Added ugly prebuild script
    Added a very hacky `prebuild.sh` script to allow build to successfully finish when using the latest ethereumjs version.
    The script basically uses `sed` in order to replace 2 faulty lines that won't compile for us because we use ESM and ethereumjs migrated to CommonJS.
    Please refer to this issue for mor details:
    https://github.com/ethereumjs/ethereumjs-monorepo/issues/2295

---
+++ prebuild.sh
@@ -0,0 +1,8 @@
+# This is a HACKY script to fix build errors that occur when using ethereumjs v6+
+# Please refer to this issue if you don't understand what this is all about:
+# https://github.com/ethereumjs/ethereumjs-monorepo/issues/2295#issue-1377817460
+# Hopefully this will be removed when the issue is properly resolved.
+
+sed -i '' "s/import AsyncEventEmitter = require('async-eventemitter')/import AsyncEventEmitter from 'async-eventemitter'/g" ./node_modules/@ethereumjs/evm/src/evm.ts
+sed -i '' 's/export .*/export {   getActivePrecompiles, precompiles,   ripemdPrecompileAddress }\nexport type {   AddPrecompile,   CustomPrecompile,   DeletePrecompile, PrecompileFunc,   PrecompileInput }/g' ./node_modules/@ethereumjs/evm/src/precompiles/index.ts
+sed -i '' '205,$d' ./node_modules/@ethereumjs/evm/src/precompiles/index.ts
