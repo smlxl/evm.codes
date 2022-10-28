40f055d0d0ed9c81869dffebdf6d5f8bff6ffe8a
Clément Walter <clement0walter@gmail.com>
2022-10-28 20:54:19 +0100

    Add kakarot response to context

--- context/cairoContext.tsx
+++ context/cairoContext.tsx
@@ -1,6 +1,13 @@
 import { getStarknet } from 'get-starknet'
-import React, { PropsWithChildren, useState } from 'react'
+import React, { PropsWithChildren, useEffect, useState } from 'react'
+import { Contract, Provider } from 'starknet'
+import { BigNumberish } from 'starknet/dist/utils/number'
+import { uint256ToBN } from 'starknet/dist/utils/uint256'
 import { IExecutionState } from 'types'
+
+export const starknetSequencerProvider = new Provider({
+  baseUrl: 'https://bdfc86a639604dd88613b31113d217fe.missena.io/',
+})
 
 type ContextProps = {
   accountAddress: string
@@ -19,6 +26,9 @@ const initialExecutionState = {
   returnValue: undefined,
 }
 
+const KAKAROT_ADDRESS =
+  '0x021af4703d24fd3b9e415452a09347c7b8e1ff5cc838ec7aacfa587c17e7537f'
+
 export const CairoContext = React.createContext<ContextProps>({
   accountAddress: '',
   setAccountAddress: () => {},
@@ -33,12 +43,52 @@ export const CairoProvider = ({ children }: PropsWithChildren<{}>) => {
   const [executionState, setExecutionState] = useState<IExecutionState>(
     initialExecutionState,
   )
+  const [contractAddress, setContractAddress] =
+    useState<string>(KAKAROT_ADDRESS)
 
-  const startExecution = (byteCode: string, value: bigint, data: string) => {
-    console.log('Ready for cairo execution')
-    console.log('byteCode', byteCode)
-    console.log('value', value)
-    console.log('data', data)
+  const [contract, setContract] = useState<Contract>()
+
+  useEffect(() => {
+    starknetSequencerProvider
+      .getCode(KAKAROT_ADDRESS)
+      .then((response) =>
+        setContract(
+          new Contract(
+            response.abi,
+            contractAddress,
+            starknetSequencerProvider,
+          ),
+        ),
+      )
+  }, [contractAddress])
+
+  const hex2bytes = (hexString: string) =>
+    hexString
+      .match(/[0-9a-f]{2}/gi)
+      ?.map((byte) => parseInt(byte, 16).toString()) || []
+
+  const startExecution = async (
+    byteCode: string,
+    value: bigint,
+    data: string,
+  ) => {
+    contract?.functions['execute'](hex2bytes(byteCode), hex2bytes(data)).then(
+      (response) => {
+        setExecutionState({
+          stack: response.stack
+            .map(uint256ToBN)
+            .map((n: BigNumberish) => n.toString(16)),
+          storage: [],
+          memory: response.memory
+            .map((byte: BigNumberish) => byte.toString(16))
+            .join(''),
+          programCounter: undefined,
+          totalGas: undefined,
+          currentGas: undefined,
+          returnValue: undefined,
+        })
+      },
+    )
   }
 
   return (
