ae93b0f102c9d287d0c19bd62d3a2d0efcd1f81f
Dragan Pilipovic <draganpilipovic1997bp@gmail.com>
2023-02-09 22:49:29 +0100

    suggestion fix

--- util/gas.ts
+++ util/gas.ts
@@ -578,14 +578,12 @@ export const calculatePrecompiledDynamicFee = (
       }
 
       if (common.gteHardfork('berlin')) {
-        result.imul(multComplexityEIP2565(maxLen))
-	result = result.divn(Gquaddivisor)
+	result.imul(multComplexityEIP2565(maxLen)).idivn(Gquaddivisor)
         if (result.ltn(200)) {
           result = new BN(200)
         }
       } else {
-        result.imul(multComplexity(maxLen))
-	result = result.divn(Gquaddivisor)
+        result.imul(multComplexity(maxLen)).idivn(Gquaddivisor)
       }
       break
     }
