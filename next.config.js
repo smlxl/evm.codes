cf39428db12e49c980ba4cccb362a1ee2264b6f9
Cheethas <47148561+cheethas@users.noreply.github.com>
2023-02-27 05:01:52 +0000

    fix: build issues

--- next.config.js
+++ next.config.js
@@ -23,6 +23,7 @@ module.exports = withPlausibleProxy()({
     })
     config.experiments = {
       asyncWebAssembly: true,
+      layers: true,
     }
 
     config.resolve.fallback = {
@@ -38,3 +39,22 @@ module.exports = withPlausibleProxy()({
     return config
   },
 })
+
+class WasmChunksFixPlugin {
+  apply(compiler) {
+    compiler.hooks.thisCompilation.tap('WasmChunksFixPlugin', (compilation) => {
+      compilation.hooks.processAssets.tap(
+        { name: 'WasmChunksFixPlugin' },
+        (assets) =>
+          Object.entries(assets).forEach(([pathname, source]) => {
+            if (!pathname.match(/\.wasm$/)) return
+            compilation.deleteAsset(pathname)
+
+            const name = pathname.split('/')[1]
+            const info = compilation.assetsInfo.get(pathname)
+            compilation.emitAsset(name, source, info)
+          }),
+      )
+    })
+  }
+}
