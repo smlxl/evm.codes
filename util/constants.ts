4f3c85b9267655492af001cff81c41dda83766ea
Tair Asim <hi@tairasim.com>
2023-05-15 11:05:14 +0200

    fix: shanghai dynamic fee is not displayed
    as we recently moved to timestamps (shanghai), while the app relies on
    blocks, the `findMatchingForkName` used in dynamic fee docs fails.
    
    this is a workaround hack to treat timestamp as block numbers until we
    get ethereumjs packages upgraded and the app working with timestamps.
    
    to be addressed in #238

--- util/constants.ts
+++ util/constants.ts
@@ -3,3 +3,7 @@ export const GITHUB_REPO_URL = 'https://github.com/smlxlio/evm.codes'
 // Currently active hardfork from the ones available:
 // See: https://github.com/ethereumjs/ethereumjs-monorepo/tree/master/packages/common/src/hardforks
 export const CURRENT_FORK = 'shanghai'
+
+export const FORKS_WITH_TIMESTAMPS: { [name: string]: number } = {
+  shanghai: 1681338455,
+}
