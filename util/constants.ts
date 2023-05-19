feba89c5ce3a774b2400b96896fcb15dbbd5aae5
Micha Berdichevsky <berdim99@gmail.com>
2023-05-19 13:31:11 -0400

    Rename org name

--- util/constants.ts
+++ util/constants.ts
@@ -1,4 +1,4 @@
-export const GITHUB_REPO_URL = 'https://github.com/smlxlio/evm.codes'
+export const GITHUB_REPO_URL = 'https://github.com/smlxl/evm.codes'
 
 // Currently active hardfork from the ones available:
 // See: https://github.com/ethereumjs/ethereumjs-monorepo/tree/master/packages/common/src/hardforks
