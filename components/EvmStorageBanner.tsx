63fee71d7bae2ac7d17503b900f774eb0e3d8975
Shane Auerbach <shaneauerbach@gmail.com>
2023-07-12 17:16:26 -0700

    fixes minor copy issue

--- components/EvmStorageBanner.tsx
+++ components/EvmStorageBanner.tsx
@@ -1,24 +1,24 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect } from 'react';
 
 const EvmStorageBanner = () => {
-  const [isShown, setIsShown] = useState(false)
+  const [isShown, setIsShown] = useState(false);
 
   useEffect(() => {
     // Check if the banner was closed previously
-    const isBannerClosed = localStorage.getItem('isBannerClosed')
+    const isBannerClosed = localStorage.getItem('isBannerClosed');
     if (!isBannerClosed) {
-      setIsShown(true)
+      setIsShown(true);
     }
-  }, [])
+  }, []);
 
   const handleCloseBanner = () => {
     // Save the state in local storage to prevent the banner from showing again
-    localStorage.setItem('isBannerClosed', 'true')
-    setIsShown(false)
-  }
+    localStorage.setItem('isBannerClosed', 'true');
+    setIsShown(false);
+  };
 
   if (!isShown) {
-    return null
+    return null;
   }
 
   return (
@@ -35,39 +35,25 @@ const EvmStorageBanner = () => {
           viewBox="0 0 24 24"
           stroke="currentColor"
         >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth={2}
-            d="M6 18L18 6M6 6l12 12"
-          />
+          <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M6 18L18 6M6 6l12 12" />
         </svg>
       </button>
       <div className="container mx-auto px-4 md:px-6">
-        <p className="font-medium md:text-xl mb-4 pt-6">
-          Introducing evm.storage
-        </p>
+        <p className="font-medium md:text-xl mb-4 pt-6">Introducing evm.storage</p>
         <p className="font-normal text-2base text-gray-400 mb-4">
-          Dive deep into the storage of any contract on Ethereum and Avalanche.
-          Explore contract storage and state at any block height.
+          Dive deep into the storage of any contract on Ethereum or Avalanche. Explore contract storage and state at any block height.
         </p>
         <div className="flex flex-row space-x-4 text-2base font-medium">
-          <a
-            className="underline text-indigo-500"
-            href="https://www.evm.storage"
-          >
+          <a className="underline text-indigo-500" href="https://www.evm.storage">
             Try it out
           </a>
-          <a
-            className="underline text-indigo-500"
-            href="https://blog.smlxl.io/introducing-evm-storage-c9fae8055286"
-          >
+          <a className="underline text-indigo-500" href="https://blog.smlxl.io/introducing-evm-storage-c9fae8055286">
             Learn more
           </a>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default EvmStorageBanner
+export default EvmStorageBanner;
