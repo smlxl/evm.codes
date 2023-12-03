ba1e227d79dabb40d6863bea8455e29f51f1f302
lmittmann <lmittmann@users.noreply.github.com>
2023-12-03 14:15:00 +0100

    make Input ref-able

--- components/ui/Input.tsx
+++ components/ui/Input.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, ForwardedRef, forwardRef } from 'react'
 
 import cn from 'classnames'
 
@@ -6,59 +6,60 @@ import { Icon } from './Icon'
 
 type Props = {
   searchable?: boolean
+  ref?: ForwardedRef<HTMLInputElement>
   className?: string
 } & React.ComponentPropsWithoutRef<'input'>
 
-export const Input: React.FC<Props> = ({
-  searchable = false,
-  onFocus,
-  onBlur,
-  className,
-  ...rest
-}) => {
-  const [isFocused, setIsFocused] = useState(false)
+export const Input: React.FC<Props> = forwardRef(
+  (
+    { searchable = false, onFocus, onBlur, className, ...rest },
+    ref: ForwardedRef<HTMLInputElement>,
+  ) => {
+    const [isFocused, setIsFocused] = useState(false)
 
-  const handleFocus = (e: any) => {
-    setIsFocused(true)
-    if (onFocus) {
-      onFocus(e)
+    const handleFocus = (e: any) => {
+      setIsFocused(true)
+      if (onFocus) {
+        onFocus(e)
+      }
     }
-  }
 
-  const handleBlur = (e: any) => {
-    setIsFocused(false)
-    if (onBlur && e) {
-      onBlur(e)
+    const handleBlur = (e: any) => {
+      setIsFocused(false)
+      if (onBlur && e) {
+        onBlur(e)
+      }
     }
-  }
 
-  return (
-    <div
-      className={cn(
-        'flex items-center rounded px-3 py-2',
-        {
-          shadow: isFocused,
-        },
-        className,
-      )}
-    >
-      <input
-        onFocus={handleFocus}
-        onBlur={handleBlur}
-        className="w-full outline-none bg-transparent dark:placeholder-black-400 text-sm"
-        {...rest}
-      />
-      {searchable && (
-        <Icon
-          name="search-line"
-          className={cn(
-            'ml-2',
-            isFocused
-              ? 'text-gray-400 dark:text-gray-300'
-              : 'text-gray-300 dark:text-gray-400',
-          )}
+    return (
+      <div
+        className={cn(
+          'flex items-center rounded px-3 py-2',
+          {
+            shadow: isFocused,
+          },
+          className,
+        )}
+      >
+        <input
+          ref={ref}
+          onFocus={handleFocus}
+          onBlur={handleBlur}
+          className="w-full outline-none bg-transparent dark:placeholder-black-400 text-sm"
+          {...rest}
         />
-      )}
-    </div>
-  )
-}
+        {searchable && (
+          <Icon
+            name="search-line"
+            className={cn(
+              'ml-2',
+              isFocused
+                ? 'text-gray-400 dark:text-gray-300'
+                : 'text-gray-300 dark:text-gray-400',
+            )}
+          />
+        )}
+      </div>
+    )
+  },
+)
