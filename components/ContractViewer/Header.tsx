f77c37878c73483edc28b45eb751bf938f44ab6e
high_byte <email@hanan.beer>
2024-01-23 22:32:57 +0200

    forgot my head

---
+++ components/ContractViewer/Header.tsx
@@ -0,0 +1,9 @@
+const ContractViewerHeader = ({ children }: any) => {
+  return (
+    <div className="w-full p-2 bg-gray-100 dark:bg-black-700 pr-6 h-16">
+      {children}
+    </div>
+  )
+}
+
+export default ContractViewerHeader
