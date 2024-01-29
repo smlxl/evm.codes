8dee6017e962fa2e8b68defaf7bfe171fe709685
codingsh <codingsh@pm.me>
2024-01-29 14:51:15 +0000

    chore(editor): fixed build types

--- components/KBar/Results.tsx
+++ components/KBar/Results.tsx
@@ -1,39 +1,29 @@
 import { useMemo } from 'react'
 
 import { KBarResults, useMatches } from 'kbar'
-import type { ActionImpl } from 'kbar'
 
 import ResultItem from './ResultItem'
 
 const NO_GROUP = 'none'
 
 const Results = () => {
-  const { results } = useMatches()
+  const groups = useMatches()
 
   const flattened = useMemo(() => {
-    const flattenActions = (actions: (string | ActionImpl)[]) => {
-      return actions.reduce((acc: any[], curr: string | ActionImpl) => {
-        if (typeof curr === 'string') {
-          acc.push(curr)
-        } else {
-          acc.push(curr)
-
-          if (curr.children && curr.children.length > 0) {
-            acc.push(...flattenActions(curr.children))
-          }
-        }
+    if (groups && Array.isArray(groups)) {
+      return groups.reduce((acc: any, curr: any) => {
+        acc.push(curr.name)
+        acc.push(...curr.actions)
         return acc
       }, [])
+    } else {
+      return []
     }
-
-    return flattenActions(results)
-  }, [results])
+  }, [groups])
 
   return (
     <KBarResults
-      items={flattened.filter(
-        (i: any) => typeof i !== 'string' || i !== NO_GROUP,
-      )}
+      items={flattened.filter((i: string) => i !== NO_GROUP)}
       onRender={({ item, active }) =>
         typeof item === 'string' ? (
           <div className="px-4 py-2 text-2xs uppercase text-gray-400 dark:text-gray-600 bg-white dark:bg-black-600">
