8ec5ebfe1c66c62f9d0ee792c558e6011709308e
high_byte <email@hanan.beer>
2024-04-07 11:41:07 +0300

    remove react-no-ssr and fix unmet peer deps

--- components/ui/ResizeablePanel.tsx
+++ components/ui/ResizeablePanel.tsx
@@ -7,6 +7,7 @@ export function cn(...inputs: string[]) {
 }
 
 const ResizablePanelGroup = ({
+  id,
   className,
   ...props
 }: React.ComponentProps<typeof ResizablePrimitive.PanelGroup>) => (
@@ -15,6 +16,7 @@ const ResizablePanelGroup = ({
       'flex h-full w-full data-[panel-group-direction=vertical]:flex-col',
       className as string,
     )}
+    id={id}
     {...props}
   />
 )
@@ -22,6 +24,7 @@ const ResizablePanelGroup = ({
 const ResizablePanel = ResizablePrimitive.Panel
 
 const ResizableHandle = ({
+  id,
   className,
   ...props
 }: React.ComponentProps<typeof ResizablePrimitive.PanelResizeHandle> & {
@@ -32,6 +35,7 @@ const ResizableHandle = ({
       'relative flex w-px border-t-2 border-l-2 items-center justify-center bg-border after:absolute after:inset-y-0 after:left-1/2 after:w-1 after:-translate-x-1/2 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring focus-visible:ring-offset-1 data-[panel-group-direction=vertical]:h-px data-[panel-group-direction=vertical]:w-full data-[panel-group-direction=vertical]:after:left-0 data-[panel-group-direction=vertical]:after:h-1 data-[panel-group-direction=vertical]:after:w-full data-[panel-group-direction=vertical]:after:-translate-y-1/2 data-[panel-group-direction=vertical]:after:translate-x-0 [&[data-panel-group-direction=vertical]>div]:rotate-90',
       className as string,
     )}
+    id={id}
     {...props}
   ></ResizablePrimitive.PanelResizeHandle>
 )
