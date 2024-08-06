23eefe8fae573eb6ea4b41adfb7198c9b85a6b91
hangleang <hangleang127@gmail.com>
2024-08-06 17:13:16 +0700

    feat: add tooltip on EOF toggle

--- components/EOFToggle.tsx
+++ components/EOFToggle.tsx
@@ -2,6 +2,7 @@ import { useContext, useCallback } from 'react'
 
 import Image from 'next/image'
 import eofIcon from 'public/eof_icon.png'
+import { Tooltip } from 'react-tooltip'
 
 import { EthereumContext } from 'context/ethereumContext'
 
@@ -17,13 +18,19 @@ const EOFToggle = () => {
   return (
     <div className="flex justify-end items-center rounded">
       <div className="flex items-center mr-2">
-        <Image alt="EOF icon" src={eofIcon} width={30} height={30} />
-
-        <Toggle
-          id="eof-toggle"
-          onChange={handleEOFToggle}
-          isChecked={showEOF}
-        />
+        <span
+          className="flex items-center pl-2 text-gray-400 dark:text-black-400"
+          data-tooltip-content="Toggle viewing EOF opcodes"
+          data-tooltip-id={`tip-toggle-eof`}
+        >
+          <Image alt="EOF icon" src={eofIcon} width={30} height={30} />
+          <Tooltip className="tooltip" id={`tip-toggle-eof`} />
+          <Toggle
+            id="eof-toggle"
+            onChange={handleEOFToggle}
+            isChecked={showEOF}
+          />
+        </span>
       </div>
     </div>
   )
