be4ab3df79a5c214efa911db6ea3ce766fafe3fb
hangleang <hangleang127@gmail.com>
2024-08-07 09:05:57 +0700

    fix: toggle styling

--- components/ui/Toggle.tsx
+++ components/ui/Toggle.tsx
@@ -28,7 +28,7 @@ export const Toggle: React.FC<Props> = ({
       <label htmlFor={id} className="hidden">
         {''}
       </label>
-      <div className="peer h-6 w-11 rounded-full bg-gray-200 dark:bg-gray-900 border-2 after:absolute after:left-[3px] after:top-0.5 after:h-5 after:w-5 after:rounded-full after:bg-white after:transition-all after:content-[''] peer-checked:bg-indigo-800 peer-checked:after:translate-x-[1.1rem]"></div>
+      <div className="peer h-6 w-11 rounded-full bg-gray-200 dark:bg-gray-900 border-2 after:absolute after:left-[5px] after:top-1 after:h-4 after:w-4 after:rounded-full after:bg-white after:transition-all after:content-[''] peer-checked:bg-indigo-800 peer-checked:after:translate-x-[1.1rem]"></div>
       {text && (
         <span className="ml-2 text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white">
           {text}
