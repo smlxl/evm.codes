7fc09f385ed9f1136f0ae0c9c1b9b6ce9dcb9152
Brage <5640782+2xic@users.noreply.github.com>
2024-08-30 17:17:12 +0200

    Rename 'EOF' -> 'EOF (Prague)'

--- util/constants.ts
+++ util/constants.ts
@@ -4,7 +4,7 @@ export const GITHUB_REPO_URL = 'https://github.com/smlxl/evm.codes'
 // See: https://github.com/ethereumjs/ethereumjs-monorepo/tree/master/packages/common/src/hardforks
 export const CURRENT_FORK = 'cancun'
 export const EOF_ENABLED_FORK = 'prague'
-export const EOF_FORK_NAME = 'EOF'
+export const EOF_FORK_NAME = 'EOF (Prague)'
 
 export const FORKS_WITH_TIMESTAMPS: { [name: string]: number } = {
   shanghai: 1681338455,
